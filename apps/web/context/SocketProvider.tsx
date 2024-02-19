65bcf623f2acfb97243eb2e53b068df61bbb4d07
Anandu-Vijayan <ananduvijayan8@gmail.com>
2024-02-19 12:07:39 +0530

    chat Functions fixed

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import React, { useCallback, useEffect } from "react";
-import { io } from "socket.io-client";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { io,Socket } from "socket.io-client";
 
 // fc = functional components
 
@@ -15,15 +15,37 @@ interface ISocketContext {
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
 
+export const useSocket =  () =>{
+    const state = useContext(SocketContext);
+    if(!state) throw new Error(`state is undefined`);
+
+    return state ;
+}
+
+
+
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+
+const [socket,setSocket] = useState <Socket>()
+
+
   const sendMessage: ISocketContext["sendMessage"] = useCallback((msg) => {
     console.log("Send Message: ", msg);
-  }, []);
+    if(socket){
+        socket.emit('event:message',{message:msg});
+    }
+
+
+
+
+  }, [socket]);
   useEffect(() => {
     const _socket = io("http://localhost:8080");
+    setSocket(_socket)
 
     return () => {
       _socket.disconnect();
+      setSocket(undefined)
     };
   }, []);
 

ca57821b1bbd15a032b617bf2e81118c8d8c631f
Anandu-Vijayan <ananduvijayan8@gmail.com>
2024-02-19 19:25:47 +0530

    Project completed

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,35 +1,54 @@
 import { Server } from "socket.io";
+import Redis from "ioredis";
+
+const pub = new Redis({
+  host: "redis-1f1bd39a-ananduvijayan8-1197.a.aivencloud.com",
+  port: 15506,
+  username: "default",
+  password: "AVNS_M7kTFre9NFnHMYozKae",
+});
+
+const sub = new Redis({
+  host: "redis-1f1bd39a-ananduvijayan8-1197.a.aivencloud.com",
+  port: 15506,
+  username: "default",
+  password: "AVNS_M7kTFre9NFnHMYozKae",
+});
 
 class SocketService {
   private _io: Server;
   constructor() {
     console.log("init Socket service ......");
 
-    this._io = new Server(
-      {
-        cors:{
-          allowedHeaders:['*'],
-          origin:"*",
-        }
-      }
-    );
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
+    sub.subscribe('MESSAGE')
   }
 
-  public initListeners(){
+  public initListeners() {
     const io = this.io;
-    console.log('Init Socket Listeners...');
-    
-    io.on('connect',(socket)=>{
-        console.log(`New socket connected`,socket.id);
-        socket.on('event:message',async({message}:{message:string})=>{
-            console.log("New Message Rec.",message);
-            
-        })
-        
+    console.log("Init Socket Listeners...");
+
+    io.on("connect", (socket) => {
+      console.log(`New socket connected`, socket.id);
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("New Message Rec.", message);
+        //publish this message to redis
+        await pub.publish("MESSAGE",JSON.stringify(message));
+      });
+    });
+    sub.on('message',(channel,message)=>{
+      if(channel === 'MESSAGE'){
+        io.emit('message',message)
+      }
     })
   }
 
-  get io(){
+  get io() {
     return this._io;
   }
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,34 @@
-'use client'
-import { useState } from 'react';
-import { useSocket } from '../context/SocketProvider'
-import classes from './page.module.css'
+"use client";
+import { useState } from "react";
+import { useSocket } from "../context/SocketProvider";
+import classes from "./page.module.css";
 
-export default function page() {
-  const {sendMessage} = useSocket();
-  const [message,setMessage] = useState('');
+export default function Page() {
+  const { sendMessage, messages } = useSocket();
+  const [message, setMessage] = useState("");
+  // console.log(messages,"scascascsasc");
   
+
   return (
     <div>
       <div>
-        <h1>All message show Here</h1>
+        <input
+          onChange={(e) => setMessage(e.target.value)}
+          className={classes["chat-input"]}
+          placeholder="Message..."
+        />
+        <button
+          onClick={(e) => sendMessage(message)}
+          className={classes["button"]}
+        >
+          Send
+        </button>
       </div>
       <div>
-        <input  onChange={e => setMessage(e.target.value)}  className={classes['chat-input']} type="text" placeholder="Message" />
-        <button onClick={e => sendMessage(message) } className= {classes['button']}>Send</button>
+        {messages.map((e) => (
+          <li>{e}</li>
+        ))}
       </div>
     </div>
-  )
+  );
 }
